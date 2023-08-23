29e5f4f4f146d1d64a93b40c59e3e97b6adefd38
Kim Jae Hwi <99602963+MountainBird0@users.noreply.github.com>
2023-08-23 17:23:33 +0900

    2로 변경
    a

--- Assets/00 ATest/Test.cs
+++ Assets/00 ATest/Test.cs
@@ -9,7 +9,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        a = 2;
     }
 
     // Update is called once per frame
